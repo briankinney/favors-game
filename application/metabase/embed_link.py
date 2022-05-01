6ad20fd997b0d6a75f40895fbf01f06962f6cfe4
Brian Kinney <briankanokinney@gmail.com>
2022-05-01 16:02:54 -0400

    Lock the dashboard id; send game id to metabase

--- application/metabase/embed_link.py
+++ application/metabase/embed_link.py
@@ -4,11 +4,13 @@ import time
 METABASE_SITE_URL = "https://metabase.komodote.ch"
 METABASE_SECRET_KEY = "e6aaaaf3b3e03a4decc848226b4ac41f82a414ea79bf9dbadc221e5251d3b6ae"
 
+GAME_REPORT_DASHBOARD_ID = 30
 
-def get_dashboard_embed(dashboard_id):
+
+def get_dashboard_embed(game_id):
     payload = {
-        "resource": {"dashboard": dashboard_id},
-        "params": {},
+        "resource": {"dashboard": GAME_REPORT_DASHBOARD_ID},
+        "params": {"id": game_id},
         "exp": round(time.time()) + (60 * 10)  # 10 minute expiration
     }
     token = jwt.encode(payload, METABASE_SECRET_KEY, algorithm="HS256")
