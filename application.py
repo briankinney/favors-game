be6f619ce06b1c43ec84b2b39d6a74b2ab3f52f9
luseleafpaper <lu.irena.wang@gmail.com>
2022-05-01 15:46:33 -0400

    connect to database, basic data objs for play

--- application.py
+++ application.py
@@ -3,6 +3,7 @@
 import os
 import json
 import sqlalchemy
+from application.models import *
 import datetime
 import zipfile
 import random
@@ -16,42 +17,50 @@ from io import BytesIO
 app = Flask(__name__)
 app.config['TESTING'] = True
 
-DATABASE_URL = f"postgresql://something or other"
-engine = sqlalchemy.create_engine(DATABASE_URL)
-
 
 # Route to homepage
 @app.route("/", methods=['GET'])
 def render_index():
     return render_template('home.html', page_title="Home")
 
+
 @app.route("/game/create", methods=['GET'])
 def render_game_create():
     return render_template('create.html', page_title="Create a game")
+
 
 @app.route("/game/create", methods=['POST'])
 def create_game():
     pass
 
+
 @app.route("/game/<id>/join", methods=['GET'])
 def render_game_join(id):
     return render_template('join.html', page_title="Join game")
+
 
 @app.route("/game/<id>/join", methods=['POST'])
 def join_game(id):
     pass
 
+
 @app.route("/game/<id>/play", methods=['GET'])
 def render_game_play(id):
-    return render_template('play.html', page_title="The Favors Game™")
+    players = get_players()
+    favors = get_favors()
+    my_favors = get_my_favors()
+    return render_template('play.html', page_title="The Favors Game™", players=players, favors=favors, my_favors=my_favors)
+
 
 @app.route("/game/<id>/report", methods=['GET'])
 def render_game_report(id):
     return render_template('report.html', page_title=f"Game {id} results")
 
+
 @app.route("/game/<id>/exchange/create", methods=['POST'])
 def exchange_favor(id):
     pass
+
 
 # Necessary to run app if app.py is executed as a script
 if __name__ == "__main__":
---
+++ application/models/__init__.py
@@ -0,0 +1,32 @@
+import sqlalchemy
+from sqlalchemy import text
+
+DATABASE_URL = f"postgresql://favors_game@kt-dev.ca05z1awppqr.us-east-1.rds.amazonaws.com:5432/favors_game"
+engine = sqlalchemy.create_engine(DATABASE_URL)
+
+
+def get_players():
+    sql = 'SELECT * FROM players;'
+
+    with engine.connect() as conn:
+        result = conn.execute(text(sql))
+        print(result)
+        return result
+
+
+def get_favors():
+    sql = 'SELECT * FROM favors;'
+
+    with engine.connect() as conn:
+        result = conn.execute(text(sql))
+        print(result)
+        return result
+
+
+def get_my_favors(user_id):
+    sql = 'SELECT * FROM exchanges'  # TODO add filter of giving player id
+
+    with engine.connect() as conn:
+        result = conn.execute(text(sql))
+        print(result)
+        return result
